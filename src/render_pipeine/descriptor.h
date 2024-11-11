8f08043d9424108192bd967c18e0e4430c7a65d5
mtzkyr1023 <mtzkyr1023@gmail.com>
2024-11-11 23:27:50 +0900

    途中

---
+++ src/render_pipeine/descriptor.h
@@ -0,0 +1,12 @@
+﻿#ifndef _DESCRIPTOR_H_
+#define _DESCRIPTOR_H_
+
+#include <vulkan/vulkan.hpp>
+
+class DescriptorSetLayout {
+public:
+	DescriptorSetLayout() {}
+	~DescriptorSetLayout() {}
+};
+
+#endif
